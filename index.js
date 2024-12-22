67d7d038f6ee2569dc2c3f1c8fd6a3d2c58addb8
fortunat81 <163460178+fortunat81@users.noreply.github.com>
2024-12-22 03:22:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/whoami",(req,res)=>{
+  console.log(req.headers);
+  let ip = req.headers["x-forwarded-for"];
+
+  res.json({ipaddress:ip,language:req.headers["accept-language"]});
+})
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
